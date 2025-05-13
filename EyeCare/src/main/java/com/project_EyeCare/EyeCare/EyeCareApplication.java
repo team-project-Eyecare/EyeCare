2d43d4393e6c75d989a1a0d046228564c7d43d4d
DESKTOP-09IDEIE\user <hwa8078678@gmail.com>
2025-05-13 23:58:05 +0900

    메인페이지

---
+++ EyeCare/src/main/java/com/project_EyeCare/EyeCare/EyeCareApplication.java
@@ -0,0 +1,13 @@
+package com.project_EyeCare.EyeCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyeCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EyeCareApplication.class, args);
+	}
+
+}
